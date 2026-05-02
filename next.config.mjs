398446512bd61abfb51c82e941b4544edaab73f4
Anthony Cavuoti <46980442+Ant3ney@users.noreply.github.com>
2026-05-02 01:41:21 +0000

    Update config to allow for big file uploads

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Keep defaults that work well on Vercel. No Edge runtime is used for the image route.
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '30mb',
+		},
+	},
 };
 
 export default nextConfig;
